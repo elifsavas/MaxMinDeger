26220aa25c7a1300c76e27944aae5937dea5780b
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-15 14:48:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int k, l, maxNum = 0, minNum = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç Tane Sayı Gireceksiniz? :");
+        k = input.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            System.out.print(i + ". Sayıyı Giriniz: ");
+            l = input.nextInt();
+
+            if (i==1) {
+                maxNum = l ;
+                minNum = l ;
+            }
+
+            if (l< minNum ) {
+                minNum = l ;
+            } else if (l>maxNum) {
+                maxNum = l;
+            }
+
+        }
+
+        System.out.println("En Büyük Sayı:" + maxNum);
+        System.out.println("En Küçük Sayı" + minNum);
+
     }
+
+
+
+
 }
